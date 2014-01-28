27689aa1ee1a07871b330433c466ece572317943
Scott MacDonald <hello.scott@gmail.com>
2014-01-28 23:34:18 +0000

    Day 2 - arrays

--- Execute/EXEListViewController.m
+++ Execute/EXEListViewController.m
@@ -10,6 +10,8 @@
 
 @interface EXEListViewController ()
 
+@property (nonatomic) NSMutableArray *tasks;
+
 @end
 
 @implementation EXEListViewController
@@ -18,6 +20,27 @@
     [super viewDidLoad];
     
     self.title = @"Execute";
+    [self.tableView registerClass:[UITableViewCell class]  forCellReuseIdentifier:@"cell"];
+    
+    UITextField *textfield = [[UITextField alloc] initWithFrame:CGRectMake(0.0f, 0.0f, 280.0f, 32.0f)];
+    textfield.returnKeyType = UIReturnKeyGo;
+    textfield.backgroundColor = [UIColor colorWithWhite:0.90f alpha:1.0f];
+    textfield.placeholder = @"what do you want to execute?";
+    self.navigationItem.titleView = textfield;
+    
+    self.tasks = [[NSMutableArray alloc] init];
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return [self.tasks count];
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"cell" forIndexPath:indexPath];
+    
+    cell.textLabel.text = self.tasks[indexPath.row];
+    
+    return cell;
 }
 
 
